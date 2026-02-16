67b7eb8d0b048ecc2486983c3e7917369e820a22
hashiryu1408-creator <hashiryu1408@gmail.com>
2026-02-16 10:28:45 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+$(function(){
+    $(".toggle_btn").on("click", function () {
+        $("header").toggleClass("open");
+    });
+
+    $("#navi a").on("click", function () {
+        $("header").toggleClass("open");
+    });
+
+    $(function(){
+        $('a[href^="#"]').click(function(){
+            let href = $(this).attr("href");
+            let target = $ (href == "#" || href == "" ? "html" : href);
+            let position = target.offset().top;
+            $("html, body").animate({scrollTop: position}, 600, "swing");
+            return false;
+        });
+    });
+});
+
